77a52401a94c42d165f009b56ec072d45a6762e9
logarithms17 <142600036+logarithms17@users.noreply.github.com>
2024-08-21 11:04:42 +0800

    Update authSlice.js

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -62,9 +62,9 @@ const authSlice = createSlice({
                 state.isRefreshing = false;
             })
             .addCase(updateAvatar.fulfilled, (state, action) => {
-    state.user.avatarUrl = action.payload.avatarUrl; // Store the avatar URL as a string
-    console.log(state.user.avatarUrl); // Log the updated avatar URL
-})
+                state.user.avatarUrl = action.payload.avatarUrl; // Store the avatar URL as a string
+                console.log(state.user.avatarUrl); // Log the updated avatar URL
+            })
             .addCase(updateAvatar.rejected, (state, action) => {
                 console.log(action);
             })
